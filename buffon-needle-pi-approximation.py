4723fa72b5fa2172e7dc9b7ac56ace729046cb1f
Axel2Boni <axel.deboni@edhec.com>
2025-10-20 21:56:20 +0200

    Enhance clarity and appearance of plots

--- buffon-needle-pi-approximation.py
+++ buffon-needle-pi-approximation.py
@@ -3,11 +3,12 @@ import numpy.random as rd
 import matplotlib.pyplot as plt
 
 for i in range(0, 11):
-    plt.plot([i, i], [0, 10], color='black')
+    plt.plot([i, i], [-1, 11], color='black')
 
-counter = 0
 nb_needle = 10000
 perc_needle_displayed = 0.05
+
+counter = 0
 for i in range(0, nb_needle):
     X1 = rd.uniform(0, 10)
     X2 = rd.uniform(0, 10)
@@ -25,14 +26,23 @@ for i in range(0, nb_needle):
         counter += 1
         if p < perc_needle_displayed:
             plt.plot([X1, Y1], [X2, Y2], color='red')
-plt.figure()
 
 # Results
-# with counter/nb_needle approx. 2/np.pi
-approx_pi = 2*(1/(counter/nb_needle))
+# with counter/nb_needle ≈ 2/np.pi
+approx_pi = 2*nb_needle/counter
 print('An approximation of pi is given by: ', approx_pi)
 print('Error: ', np.pi - approx_pi)
 print('Percentage of needle displayed: ', perc_needle_displayed*100, '%')
+
+plt.plot([], [], color='red', label='Needle crossing a line')
+plt.plot([], [], color='black', label='Needle not crossing a line')
+plt.title(f"Approximation of π using Buffon's needle method\n{
+          nb_needle} simulated needles ({perc_needle_displayed*100}% displayed)\n\
+        π ≈ {approx_pi}")
+plt.legend()
+plt.xticks(range(0, 11))
+plt.yticks([])
+plt.figure()
 
 
 # Calculation of errors
@@ -52,27 +62,29 @@ def approx_pi_with_n_needle(n):
     return approx_pi
 
 
-n = np.array([10, 30, 100] + [10**j*i for j in range(2, 5)
+n = np.array([10, 30, 50, 100]+[10**j*i for j in range(2, 4)
              for i in range(2, 11)])
 list_approx_pi = np.array([approx_pi_with_n_needle(k) for k in n])
 list_error_pi = np.abs(list_approx_pi - np.pi)
 
-plt.plot(n[:12], list_error_pi[:12], color='black',
+plt.plot(n, list_error_pi, color='black',
          label='Approximation error')
-plt.plot([0, n[11]], [0, 0], color='red', linewidth='2', linestyle='--')
+plt.plot([0, n[-1]], [0, 0], color='red', linewidth='2', linestyle='--')
+plt.title("Error in π approximation relative to number of needles")
 plt.legend()
 plt.figure()
-
 
 # Error negligibility analysis
 plt.loglog(n, list_error_pi, color='black', label='Approximation error')
 negl_index1 = 1/3
-plt.loglog(n, 1/n**negl_index1, color='turquoise', linewidth='2',
+plt.loglog(n, 1/n**negl_index1, color='royalblue', linewidth='3',
            linestyle=':', label=rf"$n^{{-{negl_index1:.2f}}}$")
 negl_index2 = 1/2
-plt.loglog(n, 1/n**negl_index2, color='darkcyan', linewidth='3',
-           linestyle=':', label=rf"$n^{{-{negl_index2}}}$")
+plt.loglog(n, 1/n**negl_index2, color='crimson', linewidth='3',
+           linestyle='-', label=rf"$n^{{-{negl_index2}}}$")
 negl_index3 = 1
-plt.loglog(n, 1/n**negl_index3, color='darkslategray', linewidth='2',
+plt.loglog(n, 1/n**negl_index3, color='seagreen', linewidth='3',
            linestyle=':', label=rf"$n^{{-{negl_index3}}}$")
+plt.title(
+    "Rate of convergence of approximation error in π estimation using a log-log plot")
 plt.legend()
