b1c4599707e765794d344346642102d0ae23a33f
Axel2Boni <axel.deboni@edhec.com>
2025-10-22 11:42:14 +0200

    Polish visualizations and update labels

--- box-muller-gaussian-generator.py
+++ box-muller-gaussian-generator.py
@@ -17,10 +17,11 @@ theta = rd.uniform(0, 2*np.pi, n)
 X1 = np.sqrt(R)*np.cos(theta)
 Y1 = np.sqrt(R)*np.sin(theta)
 
-plt.plot(x, pdf, color='black', label='Th. PDF')
+plt.plot(x, pdf, color='black', label='Theoretical PDF')
 plt.hist(X1, bins=100, density=True, color='red',
-         label='Box-Müller approach1')
-plt.legend()
+         label='Box–Müller (approach 1)')
+plt.title("Normal distribution generated using Box-Müller method")
+plt.legend(loc='center left', bbox_to_anchor=(0.6, 0.87))
 
 
 # Approach 2: Box-Müller variant using only uniform random variables in [0, 1]
@@ -31,10 +32,12 @@ R = -2*np.log(U)
 theta = V*2*np.pi
 X2 = np.sqrt(R)*np.cos(theta)
 Y2 = np.sqrt(R)*np.sin(theta)
-plt.plot(x, pdf, color='black', label='Th. PDF')
-plt.hist(X2, bins=100, density=True, color='red', label='Box-Müller approach2')
-plt.legend()
-
+plt.figure()
+plt.plot(x, pdf, color='black', label='Theoretical PDF')
+plt.hist(X2, bins=100, density=True, color='red',
+         label='Box–Müller (approach 2)')
+plt.title("Normal distribution generated using Box-Müller method")
+plt.legend(loc='center left', bbox_to_anchor=(0.6, 0.87))
 
 # Empirical study of variable independence using a scatter plot
 # -> Computation of the R² (coefficient of determination)
@@ -45,17 +48,18 @@ model.fit(X2_mod, Y2_mod)
 Y2_pred = model.predict(X2_mod)
 r2 = r2_score(Y2, Y2_pred)
 
-slope = float(model.coef_[0])
-intercept = float(model.intercept_)
+slope = model.coef_.item()
+intercept = model.intercept_.item()
 plt.figure()
 plt.scatter(X2, Y2, s=10, color='blue', label='Data points')
-plt.plot(X2, Y2_pred, color='red', label=f"Regression line: Y = {
-         slope:.3f}x + {intercept:.3f}, R² = {r2:.7f}")
+plt.plot(X2, Y2_pred, color='red', linewidth=2, label=f"Regression line: y = {
+         slope:.3f}x + {intercept:.3f}")
 
-plt.xlabel('Values of X2')
-plt.ylabel('Values of Y2')
+plt.xlabel('Values of X')
+plt.ylabel('Values of Y')
 plt.axis('equal')
-plt.title('Scatter plot for empirical study of variable independence')
+plt.title(
+    f"Scatter plot for empirical study of variable independence\nR² = {r2:.7f}")
 plt.legend()
 
 
@@ -91,11 +95,36 @@ for i in range(len(radius)):
     perc += 1/nb_quantile * 100
 
     plt.plot([], [], color=colors[i], linestyle=':', linewidth=5,
-             label=f"Radius: {radius[i]:.5f}\nPoints included:{nb_point}\
+             label=f"Points included: {nb_point}\
              \nPercentage included: {perc:.1f}%")
-    ax.legend(loc='center left', bbox_to_anchor=(0.8, 0.87))
+    ax.set_xlabel("X coordinate")
+    ax.set_ylabel("Y coordinate")
+    plt.title(f"Radial distribution visualization — Circle {
+              i+1} (Radius = {radius[i]:.5f})")
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.87))
     # ---
 
     plt.xlim(-radius[i]*1.1, radius[i]*1.1)
     plt.ylim(-radius[i]*1.1, radius[i]*1.1)
     plt.pause(1)
+
+# Creation of the final visualization
+plt.figure()
+
+fig, ax = plt.subplots()
+ax.scatter(X2, Y2, s=5, color='black', label='Data points')
+ax.set_aspect('equal', adjustable='box')
+colors = cm.autumn(np.linspace(0, 1, len(radius)))
+
+for r, c in zip(radius, colors):
+    circle = plt.Circle((0, 0), r, fill=False, color=c,
+                        linewidth=3, linestyle=':')
+    ax.add_artist(circle)
+
+# Title and legend ---
+ax.set_xlabel("X coordinate")
+ax.set_ylabel("Y coordinate")
+plt.title(f"Radial distribution — Final visualization (Max distance = {
+          dist[-1]:.5f})")
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.87))
+# ---
